4fd756ec1f960530f151262051348524aa3dc385
MarinEstrada <adrian.marin.estrada@gmail.com>
2021-06-01 22:50:55 -0500

    Finished emulating main page
    Next steps: add state functionality to change book from one shelf to
    another

--- src/Book.js
+++ src/Book.js
@@ -24,6 +24,15 @@ class Book extends React.Component {
                             backgroundImage: `url(${book.imageLinks.thumbnail})`
                         }}
                     ></div>
+                    <div className='book-shelf-changer'>
+                        <select>
+                            <option value='move' disabled>Move to...</option>
+                            <option value='currentlyReading'>Currently Reading</option>
+                            <option value='wantToRead'>Want to Read</option>
+                            <option value='read'>Read</option>
+                            <option value='none'>Remove</option>
+                        </select>
+                    </div>
                 </div>
             </div>
         )
