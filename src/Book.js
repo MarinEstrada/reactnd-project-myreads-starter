b4e3cbc4c9ce0c1851e90e588ed4088a67b02e4c
MarinEstrada <adrian.marin.estrada@gmail.com>
2021-06-01 20:33:09 -0500

    Render book cover
    done in Book.js

--- src/Book.js
+++ src/Book.js
@@ -6,16 +6,24 @@ import './css/App.css'
 class Book extends React.Component {
 
     static propTypes = {
-        books: PropTypes.object.isRequired,
+        book: PropTypes.object.isRequired,
     }
 
     render () {
 
-        const { books } = this.props
+        const { book } = this.props
 
         return (
             <div className='book'>
                 <div className='book-top'>
+                    <div
+                        className='book-cover'
+                        style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: book.infolink
+                        }}
+                    ></div>
                 </div>
             </div>
         )
