97ded62f53c5e47a6ecad36a55b63ce24b90e590
MarinEstrada <adrian.marin.estrada@gmail.com>
2021-06-03 15:58:30 -0500

    Add functionality to back button from /search to /

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './css/App.css'
+import { Link } from 'react-router-dom'
 
 
 class Search extends React.Component{
@@ -9,7 +10,9 @@ class Search extends React.Component{
         return (
             <div className='search-books'>
                 <div className='search-books-bar'>
-                    <button className='close-search' >Close</button>
+                    <Link className='close-search' to='/'>
+                        <button className='close-search' >Close</button>
+                    </Link>
                     <div className='search-books-input-wrapper'>
                         <input type='text' placeholder='Search by Title or Author'/>
                     </div>
